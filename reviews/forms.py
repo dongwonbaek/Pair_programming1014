a6a0d35668ae55a9386353dd6ef8de52f333d974
JC <chohy1106@gmail.com>
2022-10-14 15:44:17 +0900

    make reviews/create,detail

---
+++ reviews/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Review
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = [
+            "title",
+            "content",
+            "movie_name",
+            "grade",
+        ]
---
+++ reviews/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from .models import Review
+from .forms import ReviewForm
+
+# Create your views here.
+def index(request):
+    reviews = Review.objects.order_by("-pk")
+    context = {
+        "reviews": reviews,
+    }
+    return render(request, "reviews/index.html", context)
+
+
+def create(request):
+    if request.method == "POST":
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            review_form.save()
+            return redirect("reviews:index")
+    else:
+        review_form = ReviewForm()
+    context = {
+        "review_form": review_form,
+    }
+    return render(request, "reviews/create.html", context=context)
+
+
+def detail(request, pk):
+    review = Review.objects.get(pk=pk)
+    context = {
+        "review": review,
+    }
+    return render(request, "reviews/detail.html", context)
