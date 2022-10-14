6fff371d16bf2bce5d4e29c8f157c88b626f2cc0
dongwonbaek <bmrio100@gmail.com>
2022-10-14 11:03:04 +0900

    accounts/login

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import redirect, render
 from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from .models import User
 # Create your views here.
 def index(request):
-    return render(request, 'accounts/index.html')
+    users = User.objects.all()
+    context = {
+        'users':users,
+    }
+    return render(request, 'accounts/index.html', context)
 
 def signup(request):
     if request.method == 'POST':
@@ -18,3 +25,20 @@ def signup(request):
         'form': form,
     }
     return render(request, 'accounts/signup.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or 'accounts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:index')
